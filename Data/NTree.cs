fd8245f82cc36fb4790b954bceb22dfd8bf1d32c
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 13:15:15 +0200

    All buildable, every in separate file

--- Data/NTree.cs
+++ Data/NTree.cs
@@ -1,6 +1,6 @@
 
 namespace SunamoData;
-public delegate void TreeVisitor<T>(T nodeData);
+//public delegate void Action<T>(T nodeData);
 
 //public delegate void A();
 
@@ -31,7 +31,7 @@ public class NTree<T>
                 return n;
         return null;
     }
-    public void Traverse(NTree<T> node, TreeVisitor<T> visitor)
+    public void Traverse(NTree<T> node, Action<T> visitor)
     {
         visitor(node.data);
         foreach (NTree<T> kid in node.children)
